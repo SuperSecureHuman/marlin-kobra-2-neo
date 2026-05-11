18c48981dd21d558f48cec38d4300d5750d3e4ea
SuperSecureHuman <supersecurehuman@gmail.com>
2026-05-11 16:52:15 +0530

    remove other CI, enable pressure adv

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3524,7 +3524,7 @@
    * ANET_BLACK   - Theme used for Anet ET4/5
    * :['BLUE_MARLIN', 'BLACK_MARLIN', 'ANET_BLACK']
    */
-  #define TFT_THEME BLACK_MARLIN
+  #define TFT_THEME BLUE_MARLIN
 
   //#define TFT_SHARED_IO   // I/O is shared between TFT display and other devices. Disable async data transfer.
 
